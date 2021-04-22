c4d64e98556e5c9caff902182c9e840dad869580
Ian Coleman <ian@iancoleman.io>
2021-04-22 10:29:59 +1000

    chore: remove OsRng
    BREAKING CHANGE: public api changed

--- src/lib.rs
+++ src/lib.rs
@@ -57,10 +57,8 @@ use core::{cmp::Ordering, fmt, ops};
 pub use prefix::Prefix;
 use rand::{
     distributions::{Distribution, Standard},
-    rngs::OsRng,
     Rng,
 };
-use rand_core::RngCore;
 use serde::{Deserialize, Serialize};
 use tiny_keccak::{Hasher, Sha3};
 
@@ -128,11 +126,9 @@ impl XorName {
     }
 
     /// Generate a random XorName
-    pub fn random() -> Self {
+    pub fn random<T: Rng>(rng: &mut T) -> Self {
         let mut xor = [0u8; XOR_NAME_LEN];
-        // TODO: OsRng needs to be removed + replaced to follow no-std.
-        OsRng.fill_bytes(&mut xor);
-
+        rng.fill(&mut xor);
         Self(xor)
     }
 
@@ -337,15 +333,17 @@ mod tests {
 
     #[test]
     fn create_random_xorname() {
-        let xorname: XorName = XorName::random();
-        let xorname2: XorName = XorName::random();
+        let mut rng = SmallRng::from_entropy();
+        let xorname: XorName = XorName::random(&mut rng);
+        let xorname2: XorName = XorName::random(&mut rng);
 
         assert_ne!(xorname, xorname2);
     }
 
     #[test]
     fn serialisation_xor_name() {
-        let obj_before: XorName = XorName::random();
+        let mut rng = SmallRng::from_entropy();
+        let obj_before: XorName = XorName::random(&mut rng);
         let data = serialize(&obj_before).unwrap();
         assert_eq!(data.len(), XOR_NAME_LEN);
         let obj_after: XorName = deserialize(&data).unwrap();
